db107ea536336e38a76cfefa99fa37485052f061
mxrget <2024kalinin.ma@student.letovo.ru>
2023-03-30 09:55:33 +0300

    работа над классом игрока, дальнейшие изменения описаны в файле player1.py

--- player1.py
+++ player1.py
@@ -1,4 +1,10 @@
 # player 1 code
 
 def __init__(self, overall, per_step):
-    pass
+    rocks = int(input())
+    if rocks <= per_step and rocks <= overall:
+        return rocks
+    else:
+        print('Некорректное количество камней!')
+        # короче тут надо чтобы оно просило нормальное количество камней пока не получит
+        # и еще надо сделать в testsystem саму проверку не проиграл ли ты уже случайно
--- testsystem.py
+++ testsystem.py
@@ -2,12 +2,14 @@ import player1, player2
 import random
 import math
 
-steps = 0
+steps = 1
 overall_rocks = random.randint(20, 65)
 max_per_step = math.floor(math.sqrt(overall_rocks))
 
 while overall_rocks != 0:
-    if steps % 2 == 0:
+    if steps % 2 == 1:
         p1 = player1(overall_rocks, max_per_step)
+        steps += 1
     else:
         p2 = player2(overall_rocks, max_per_step)
+        steps += 1
